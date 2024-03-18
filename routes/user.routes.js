8b7de3acca1eb824cded54a7c4acdf305e504a31
bars <154560345+bars30@users.noreply.github.com>
2024-03-18 22:20:45 +0400

    Add files via upload

---
+++ routes/user.routes.js
@@ -0,0 +1,17 @@
+// user.routes.js
+const express = require('express');
+const router = express.Router(); // Use express.Router() to create a router
+
+const userController = require('../controllers/user.controller');
+
+// Define routes
+router.post('/user', userController.createUser);
+router.get('/user', userController.getUsers);
+router.get('/user/:id', userController.getOneUser);
+router.put('/user', userController.updateUser);
+router.delete('/user/:id', userController.deleteUser);
+
+module.exports = router;
+
+
+
